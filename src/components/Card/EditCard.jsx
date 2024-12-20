d3ffd6fd6968f2486ceb46498cdce147fbb130e9
Oleksandr-Gab <120855728+Oleksandr-Gab@users.noreply.github.com>
2024-12-20 14:38:43 +0200

    Update EditCard.jsx

--- src/components/Card/EditCard.jsx
+++ src/components/Card/EditCard.jsx
@@ -1,5 +1,19 @@
+import { Box } from '@mui/material';
 import { useState } from 'react';
 import Modal from 'react-modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'var(--modal-backgr)',
+  border: 'none',
+  borderRadius: '8px',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function EditCard({ card, onSave, onClose, isEditing }) {
   const [newTitle, setNewTitle] = useState(card.title);
@@ -28,50 +42,52 @@ export default function EditCard({ card, onSave, onClose, isEditing }) {
         onRequestClose={() => onClose(false)}
         contentLabel="Edit Card"
       >
-        <h2>Edit card</h2>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            handleEditCard();
-          }}
-        >
-          <input
-            type="text"
-            value={newTitle}
-            onChange={e => setNewTitle(e.target.value)}
-            placeholder="Card title"
-            required
-          />
-          <textarea
-            value={newDescription}
-            onChange={e => setNewDescription(e.target.value)}
-            placeholder="Card description"
-            required
-          />
-          <label>
-            Priority:
-            <select
-              value={newPriority}
-              onChange={e => setNewPriority(e.target.value)}
-              required
-            >
-              <option value="Low">Low</option>
-              <option value="Medium">Medium</option>
-              <option value="High">High</option>
-            </select>
-          </label>
-          <label>
-            Deadline:
+        <Box sx={style}>
+          <h2>Edit card</h2>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              handleEditCard();
+            }}
+          >
             <input
-              type="datetime-local"
-              value={newDeadline}
-              onChange={e => setNewDeadline(e.target.value)}
+              type="text"
+              value={newTitle}
+              onChange={e => setNewTitle(e.target.value)}
+              placeholder="Card title"
               required
             />
-          </label>
-          <button type="submit">Edit</button>
-        </form>
-        <button onClick={() => onClose(false)}>Close</button>
+            <textarea
+              value={newDescription}
+              onChange={e => setNewDescription(e.target.value)}
+              placeholder="Card description"
+              required
+            />
+            <label>
+              Priority:
+              <select
+                value={newPriority}
+                onChange={e => setNewPriority(e.target.value)}
+                required
+              >
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
+              </select>
+            </label>
+            <label>
+              Deadline:
+              <input
+                type="datetime-local"
+                value={newDeadline}
+                onChange={e => setNewDeadline(e.target.value)}
+                required
+              />
+            </label>
+            <button type="submit">Edit</button>
+          </form>
+          <button onClick={() => onClose(false)}>Close</button>
+        </Box>
       </Modal>
     </div>
   );
